626e2f04c7bf1d6b9ad10195c810bf51b87f01e1
nimm2sugus <126090840+nimm2sugus@users.noreply.github.com>
2025-05-22 10:28:25 +0200

    Create StandzeitennachZahlungsart.py

---
+++ StandzeitennachZahlungsart.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Funktion zum Laden der Excel-Datei
+@st.cache_data
+def load_excel_file(uploaded_file):
+    try:
+        df = pd.read_excel(uploaded_file, engine='openpyxl')
+        return df
+    except Exception as e:
+        st.error(f"Fehler beim Laden der Datei: {e}")
+        return None
+
+# Datei-Upload
+uploaded_file = st.file_uploader("📁 Bereinigte Excel-Datei hochladen", type=["xlsx", "xls"])
+
+if uploaded_file is not None:
+    df = load_excel_file(uploaded_file)
+
+    if df is not None:
+        st.subheader("Originaldaten nach Datenformatanpassung")
+
+        df = df.copy()
+        df['Gestartet'] = pd.to_datetime(df['Gestartet'], errors='coerce')
+        df['Beendet'] = pd.to_datetime(df['Beendet'], errors='coerce')
+        df['Verbrauch_kWh'] = pd.to_numeric(df['Verbrauch (kWh)'], errors='coerce')
+        df['Kosten_EUR'] = pd.to_numeric(df['Kosten'], errors='coerce')
+        df['Ladezeit_h'] = (df['Beendet'] - df['Gestartet']).dt.total_seconds() / 3600.0
+        df['P_Schnitt'] = df['Verbrauch_kWh'] / df['Ladezeit_h']
+
+        df['Jahr'] = df['Beendet'].dt.year
+        df['Monat_num'] = df['Beendet'].dt.month
+        df['Tag'] = df['Beendet'].dt.day
+        df['Stunde'] = df['Beendet'].dt.hour
+
+        st.write(df, use_container_width=True)
+
+        grouped = df.groupby('Standortname', as_index=False).agg(
+            Verbrauch_kWh_mean=('Verbrauch_kWh', 'mean'),
+            Ladezeit_h=('Ladezeit_h', 'mean'),
+            Anzahl_Ladevorgaenge=('Verbrauch_kWh', 'count')
+        )
+        st.subheader("🔢 Allgemeine KPIs nach Standort")
+        st.dataframe(grouped, use_container_width=True)
+
+        groupedby_authtyp = df.groupby('Auth. Typ', as_index=False).agg(
+            Ladezeit_h=('Ladezeit_h', 'mean')
+        )
+        
+        st.write(groupedby_authtyp, use_container_width=True)
+
+
+
